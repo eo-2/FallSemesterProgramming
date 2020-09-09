6fa900f7863ff096734163d3ba585ac80dcc4b93
eo-2 <ohmygot7@naver.com>
2020-09-09 20:25:24 +0900

    Report2-1 A

---
+++ 02_BasicCoding/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+	
+/* main function
+* 2020.09.09
+* made by prof. Eo
+*/
+
+int add(int a, int b)
+{
+	return a + b;
+}
+int main(void)
+
+{
+	printf("Hello World! addition:(%d)\n", add(1,2));
+	system("pause..");
+	return 0;
+}
---
+++ Report 2-1 C/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* main function
+* 2020.09.09
+* made by prof. Eo
+*/
+
+
+int main(void)
+
+{
+	int a = 0;
+
+		a = a + 1;
+		a = a + 2;
+		a = a + 3;
+	printf("a : %d\n.", a);
+	system("pause..");
+
+	return 0;
+}
