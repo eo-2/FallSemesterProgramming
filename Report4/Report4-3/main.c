8d4e48ea347e624229248af1db23c3389c707075
eo-2 <ohmygot7@naver.com>
2020-09-28 21:37:22 +0900

    Report 4

--- Report4/Report4-3/main.c
+++ Report4/Report4-3/main.c
@@ -2,26 +2,49 @@
 
 int main(void)
 {
-
-
-	int i, j;
-	int row = 0;
+	int i, j, n;
 	int a = 0;
-
-	printf("정수 a: ");
-	int C = scanf("%d", &a);
-	printf("정수 row: ");
-	int D = scanf("%d", &row);
+	int row = 0;
 
 
-	for (i = 0; i < row; i++)        // i가 0부터 row값까지 증가하면서 반복
+
+	printf("정수 a와 row: ");
+	int B = scanf("%d%d", &a, &row);
+	
+
+	int nB = a / row;
+	int nC = a % row;
+
+
+	if (a > row)
 	{
-		for (j = 0; j <= a; j++)      // j가 0부터 a까지 증가하면서 a번 반복
+		for (i = 0; i < (nB - 1); i++)
+		{
+			for (j = 0; j < nB; j++)
+			{
+				printf("*");
+			}
+			printf("\n");
+		}
+		for (n = 0; n < (nB + nC); n++)
+		{
+		   printf("*");
+		}
+	}
+	else if (a == row)
+	{
+		for (i = 0; i < a; i++)
+		{
+				printf("*\n");
+		}
+	}
+	else
+	{
+		for (i = 0; i < a; i++)
 		{
 			printf("*");
 		}
-		printf("\n");                 //a번 출력한 후에 줄을 바꾼다.
-
 	}
+
 	return 0;
 }
