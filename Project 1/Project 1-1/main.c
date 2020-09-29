68dc41be0e84c9704fa38f7aef370a4d8b988f4e
eo-2 <ohmygot7@naver.com>
2020-09-29 23:33:28 +0900

    Project 1

--- Project 1/Project 1-1/main.c
+++ Project 1/Project 1-1/main.c
@@ -2,34 +2,36 @@
 
 int main(void)
 {
-	int nA = 0;
+	double nA = 0;
 
 
 	printf("각도를 입력해 주세요.: ");
-	int P = scanf("%d", &nA);
+	double P = scanf("%lf", &nA);
 
-	if (180 >= nA)
+	if ((double)180 >= nA)
 	{
-		printf("%d도 입니다.", nA);
+		printf("%.4lf도 입니다.\n", nA);
 	}
-	else if (180 < nA && nA <= 360)
+	else if (((double)180 < nA) && (nA <= (double)360))
 	{
-		int nB = nA - 360;
-		printf("%d도 입니다.", nB);
+		double nB = nA - (double)360;
+		printf("%.4lf도 입니다.\n", nB);
 	}
-	else if (360 < nA)
+	else if ((double)360 < nA)
 	{
-		int nC = nA % 360;
+		int nC = (int)nA / 360;
+		double nD = nA - (nC * 360);
+		double nE = nD - 360;
 
-		if (nC >= 180)
+		if ((0 <= nD) && (nD <= (double)180))
 		{
-			printf("%d도 입니다.", nC);
+			printf("%.4lf도 입니다.\n", nD);
 		}
-		else if (180 < nC < 360);
+		else if ((180) < nD )&& (nD <= 360))
 		{
-			int nD = nC - 180;
-			printf("%d도 입니다.", nD);
+			printf("%.4lf도 입니다.\n", nE);
 		}
+
 	}
 	return 0;
 }
